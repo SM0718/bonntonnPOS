a71f10e4ecf83d1dfa439e090e85f77f92f263d1
Sagnik Majumder <clashofclanssagnik@gmail.com>
2024-12-20 11:20:08 +0530

    New Trial

--- src/pages/productDetails/UploadProduct.jsx
+++ src/pages/productDetails/UploadProduct.jsx
@@ -74,7 +74,7 @@ function UploadProduct() {
 
     const fetchCategories = async () => {
         try {
-            const response = await fetch('api/v1/catagory/get-catagory', {
+            const response = await fetch('https://bonntonn.up.railway.app/api/v1/catagory/get-catagory', {
                 method: 'GET',
             });
 
@@ -153,7 +153,7 @@ function UploadProduct() {
                 });
             });
             console.log(productData, "--Product Data--")
-            const response = await fetch('api/v1/products/add-product', {
+            const response = await fetch('https://bonntonn.up.railway.app/api/v1/products/add-product', {
                 method: 'POST',
                 body: productData,
             });
